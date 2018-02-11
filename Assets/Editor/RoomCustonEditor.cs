eb8af9426d12c1f7432b6345b45eab18a1dfcdbe
WHEATLEY\Sebastian <lagoueyte23@hotmail.com>
2018-02-11 15:03:26 -0500

    Creación de Custom Inspector para Room
    Además de Creación de Room

---
+++ Assets/Editor/RoomCustonEditor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(Room))]
+public class RoomCustonEditor : Editor {
+
+    public Vector3 newPosition = new Vector3();
+
+    public override void OnInspectorGUI()
+    {
+        Room targetRoom = (Room)target;
+
+        base.OnInspectorGUI();
+
+        newPosition = EditorGUILayout.Vector3Field("New Position", newPosition);
+
+        if (GUILayout.Button("Change Position"))
+        {
+            targetRoom.ChangePosition(newPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Gorun Gork/Room")]
+public class Room : ScriptableObject {
+
+    public Vector3 roomPosition;
+    public string roomDescription;
+    public string roomName;
+
+    public void ChangePosition(Vector3 newRoomPosition)
+    {
+        roomPosition = newRoomPosition;
+        Debug.Log("Done");
+    }
+
+    
+
+}
