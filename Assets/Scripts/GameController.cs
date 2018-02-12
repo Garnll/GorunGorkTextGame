acf270ad56fcdceeae5de5b55b36f663c34739c4
WHEATLEY\Sebastian <lagoueyte23@hotmail.com>
2018-02-11 21:22:12 -0500

    Cambio extra en el GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -24,8 +24,6 @@ public class GameController : MonoBehaviour {
         string logAsText = string.Join("\n", actionLog.ToArray());
 
         displayText.text = logAsText;
-
-        Debug.Log("Im here");
     }
 
     public void DisplayRoomText()
