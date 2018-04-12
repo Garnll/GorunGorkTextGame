e4d6ca7fb4f27294d34e97e750a3e297a3cc245b
WHEATLEY\Sebastian <lagoueyte23@hotmail.com>
2018-04-11 20:58:42 -0500

    Diccionario de variables estáticas. Es estatica

---
+++ Assets/Scripts/Game Control/Global Variables/GlobalVariables.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalVariables {
+
+    static Dictionary<string, int> globalVariablesDictionary = new Dictionary<string, int>();
+
+    public static void AddVariable(string variableName, int start)
+    {
+        if (!globalVariablesDictionary.ContainsKey(variableName))
+        {
+            globalVariablesDictionary.Add(variableName, start);
+        }
+        else
+        {
+            Debug.LogError(variableName + " ya existe en el diccionario global.");
+        }
+    }
+
+    public static bool ContainsVariable(string variableName)
+    {
+        if (globalVariablesDictionary.ContainsKey(variableName))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static int GetVariableCurrentValue(string variableName)
+    {
+        return globalVariablesDictionary[variableName];
+    }
+
+    public static void NewVariableValue(string variableName, int newvalue)
+    {
+        if (globalVariablesDictionary.ContainsKey(variableName))
+        {
+            globalVariablesDictionary[variableName] = newvalue;
+        }
+    }
+
+}
