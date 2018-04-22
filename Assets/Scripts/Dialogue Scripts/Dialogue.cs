9bd72449a5a597ec28b8e3af30d81ba9e161eaa7
DESKTOP-HGDOKOS\Zlaster <zlaster@hotmail.com>
2018-04-22 00:08:38 -0500

    Base Diálogos

---
+++ Assets/Scripts/Dialogue Scripts/Dialogue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue {
+
+	[Space(10)]
+	public string[] keywords;
+	[TextArea] public string[] text;
+	public Dialogue[] choices;
+
+	
+
+}
---
+++ Assets/Scripts/NPC Scripts/DialogueNPC.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Gorun Gork/NPC/Dialogue NPC")]
+public class DialogueNPC : NPCTemplate {
+
+
+	public Dialogue dialogueTree;
+
+}
