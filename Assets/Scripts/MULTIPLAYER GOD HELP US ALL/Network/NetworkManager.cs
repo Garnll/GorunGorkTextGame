9c585ce2064442ccfadaf424d9205fde86dcc26e
WHEATLEY\Sebastian <lagoueyte23@hotmail.com>
2018-05-06 17:37:28 -0500

    Cambios que espero no dañen todo

---
+++ Assets/Scripts/MULTIPLAYER GOD HELP US ALL/Network/NetworkManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manager de todo los callbacks para el Networking de Photon
+/// </summary>
+public class NetworkManager : Photon.PunBehaviour {
+
+    public static NetworkManager Instance = null;
+
+    public string gameVersion = "0.1";
+    public byte maxPlayers = 10;
+
+    private void Awake()
+    {
+        Instance = this;
+        PhotonNetwork.autoJoinLobby = false;
+        PhotonNetwork.automaticallySyncScene = true;
+    }
+
+    public void ConnectToServer()
+    {
+        if (PhotonNetwork.connected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            PhotonNetwork.ConnectUsingSettings(gameVersion);
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
+    {
+        PhotonNetwork.CreateRoom(null, new RoomOptions() {MaxPlayers =  this.maxPlayers}, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Conectado a habitación. ");
+    }
+
+    public override void OnDisconnectedFromPhoton()
+    {
+        Debug.Log("Desconectado de Photon. ");
+    }
+}
---
+++ Assets/Scripts/Room Scripts/Room Responses/ConnectToServer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Room Response 
+/// </summary>
+[CreateAssetMenu(menuName = "Gorun Gork/RoomObject Responses/Connect To Server")]
+public class ConnectToServer : RoomResponse {
+
+	public override void TriggerResponse(GameController controller)
+    {
+        base.TriggerResponse(controller);
+
+        NetworkManager.Instance.ConnectToServer();
+    }
+}
