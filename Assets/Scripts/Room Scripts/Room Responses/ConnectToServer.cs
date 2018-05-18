3b7a0fe77795d2d0637c0a9f60e09e311b9157d6
Garnable <lagoueyte23@hotmail.com>
2018-05-18 13:09:46 -0500

    Intentando solucionar este problema

--- Assets/Scripts/Room Scripts/Room Responses/ConnectToServer.cs
+++ Assets/Scripts/Room Scripts/Room Responses/ConnectToServer.cs
@@ -10,6 +10,11 @@ public class ConnectToServer : RoomResponse {
 
 	public override void TriggerResponse(GameController controller)
     {
+        if (NetworkManager.Instance.connected || NetworkManager.Instance.isConnecting)
+        {
+            return;
+        }
+
         controller.StartCoroutine(controller.StopTextWhileConnecting());
 
         base.TriggerResponse(controller);
